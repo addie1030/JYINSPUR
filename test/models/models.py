8101450dff4f478ea1c218fc211267006834c08d
ccysfn <tiancaicaoyifeng@126.com>
2019-05-02 03:25:39 +0000

    050204

--- test/models/models.py
+++ test/models/models.py
@@ -2,14 +2,14 @@
 
 from odoo import models, fields, api
 
- class test(models.Model):
-     _name = 'test.test'
+class test(models.Model):
+    _name = 'test.test'
 
-     name = fields.Char()
+    name = fields.Char()
     value = fields.Integer()
-     value2 = fields.Float(compute="_value_pc", store=True)
-     description = fields.Text()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
 
-     @api.depends('value')
-     def _value_pc(self):
-         self.value2 = float(self.value) / 100
+    @api.depends('value')
+    def _value_pc(self):
+        self.value2 = float(self.value) / 100
