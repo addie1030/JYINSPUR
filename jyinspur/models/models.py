65819a841d9916d152ea24b0f2357ac39b18597c
ccysfn <tiancaicaoyifeng@126.com>
2019-05-06 01:23:25 +0000

    050602

---
+++ jyinspur/models/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class soft(models.Model):
+    _name = 'jyinspur.soft'
+
+    name = fields.Char()
+    ggxh = fields.Char()
+    price = fields.Float()
+    #price = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    #@api.depends('value')
+    #def _value_pc(self):
+        #self.value2 = float(self.value) / 100
