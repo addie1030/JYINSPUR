c4df9ccb52d6fabe7a9118c15de7d6c2988f9c37
ccysfn <tiancaicaoyifeng@126.com>
2019-05-07 07:06:48 +0000

    050701

--- qingjia/models/.ipynb_checkpoints/models-checkpoint.py
+++ qingjia/models/.ipynb_checkpoints/models-checkpoint.py
@@ -8,12 +8,13 @@ class qingjiadan(models.Model):
     name = fields.Many2one('res.users',string='申请人',required=True)
     days = fields.Integer(compute="c_days",store=True,string='天数',readonly=True)
     stardate = fields.Date(string='开始时间')
-    reason = fields.Text(string='请假事由')
     enddate = fields.Date(string='结束时间')
+    reason = fields.Text(string='请假事由')
+    
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
 #
     @api.depends('stardate','enddate')
-    def _value_pc(self):
+    def c_days(self):
         self.days = self.enddate-self.stardate
