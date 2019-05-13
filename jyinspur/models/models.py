68768911f6427f50cdf5fb4157ab881580b60df7
ccysfn <tiancaicaoyifeng@126.com>
2019-05-13 03:08:15 +0000

    05131108

--- jyinspur/models/models.py
+++ jyinspur/models/models.py
@@ -15,6 +15,7 @@ class soft(models.Model):
     qm = fields.Char(string='全名',compute='qm_compute')  #compute为调用函数名
     state = fields.Selection([('draft','草稿'),('processing','审批中'),('done','审批通过')],string='状态',default='draft',readonly=True,index=True)
     
+    #连接名称与规格型号返回至全名
     @api.depends('name','ggxh')  #填入需要的参数
     def qm_compute(self):
         self.qm = str(self.name) + '-' + str(self.ggxh)  #需要加上str()函数不然提示数据类型错误
@@ -23,10 +24,17 @@ class soft(models.Model):
     #def _value_pc(self):
         #self.value2 = float(self.value) / 100
         
+    #审批通过方法    
     @api.multi
     def button_done(self):
         self.write({'state': 'done'})
-        
+    
+    
+    #审批提交方法
     @api.multi
     def button_confirm(self):
-        self.write({'state': 'processing'})
+        self.write({'state': 'processing'})、
+        
+    @api.multi
+    def button_cancel(self):
+        self.write({'state': 'draft'})
